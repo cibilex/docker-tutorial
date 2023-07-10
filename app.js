6b24517951a4361da5df50804ad8fb737a19ac8b
Mankurt_archer3@hotmail.com <Mankurt_archer3@hotmail.com>
2023-07-10 23:01:10 +0300

    Added ioredis package

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fastify = require('fastify')
+const Redis=require('ioredis')
 const app = fastify({
   logger: true
 })
@@ -6,6 +7,16 @@ app.get('/', (req, res) => res.send('hi world from docker container'))
 
 const start = async () => {
   try {
+    const redis = new Redis({
+      port: 6379,
+      password: 'cibilex',
+      host: process.env.HOST
+    })
+
+    await redis.set('username', 'cibilex')
+
+    console.log(await redis.get('username'))
+
     await app.listen({
       port: process.env.PORT || 8080,
       host: '0.0.0.0'
